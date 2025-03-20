9d8216e7c04a0eebdad17d7d0e94ef5241c4bdcd
Savchuk Volodymyr <savchuk.v03@gmail.com>
2025-03-20 18:53:47 +0200

    Add function for sentiment analysis

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello world!")
+import nltk
+from googletrans import Translator
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+nltk.download('vader_lexicon')
+
+async def analyze_sentiment(text: str):
+    try:
+        translator = Translator()
+        translated_text = await translator.translate(text, src='ru', dest='en')
+        translated_text = translated_text.text
+        sia = SentimentIntensityAnalyzer()
+        scores = sia.polarity_scores(translated_text)
+
+        if scores['compound'] >= 0.05:
+            return "Positive"
+        elif scores['compound'] <= -0.05:
+            return "Negative"
+        else:
+            return "Neutral"
+    except:
+        raise Exception("Error during sentiment analysis")
